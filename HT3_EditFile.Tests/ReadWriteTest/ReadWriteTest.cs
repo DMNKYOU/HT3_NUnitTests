e244c6b3cae7f6d207687c85a8c2bd3b8edb8ca3
DmitriyZalesin <zalesindmitriy@gmail.com>
2021-07-14 20:16:58 +0300

    add tests for classes Manager and ReadWrite

---
+++ HT3_EditFile.Tests/ReadWriteTest/ReadWriteTest.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.IO;
+using NUnit.Framework;
+using HT3_EditFileNUnitTest;
+using HT3_EditFile.Tasks;
+using HT3_EditFile.Work_with_File;
+using NUnit;
+namespace HT3_EditFileNUnitTest.ReadWrite
+{
+    public class ReadWriteTest
+    {
+        
+        [TestCase("Hello.txt")]
+        public void CtorTest(string pathToFile)
+        {
+            Assert.Throws<FileNotFoundException>(() => new ReadWriteFile(path: pathToFile));
+        }
+
+
+        [TestCase("..\\..\\..\\File\\TextTest.txt")]
+        public void ReadFileTest(string pathToFile)
+        {
+            ReadWriteFile readWriteFile = new ReadWriteFile(pathToFile);
+            Tuple<string, DateTime> resaultWorkFile = readWriteFile.ReadFileTakeResultAndWhenChangeFileWhichRead();
+            Assert.NotNull(resaultWorkFile.Item1);
+        }
+
+        
+        [TestCase("..\\..\\..\\File\\TextTest.txt")]
+        public void WriteFileTest(string pathToFile)
+        {
+            ReadWriteFile readWriteFile = new ReadWriteFile(pathToFile);
+            Assert.True(readWriteFile.WriteFile("Hello, Test!"));
+        }
+    }
+}
